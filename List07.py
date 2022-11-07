b193653ac94a36ab82252542bd369016e2064ba8
sobirismatov <name@uzcoder96@gmail.com>
2022-11-07 21:57:16 +0500

    07

--- List07.py
+++ List07.py
@@ -6,4 +6,9 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    i=0
+    while i<5:
+        if list1[i]==0:
+            list1[i]=False
+        i+=1
+    return list1
